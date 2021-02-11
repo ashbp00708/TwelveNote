2af8222f22f6f1911acf9234f562e99dbee68f5d
ashbp00708 <73564911+ashbp00708@users.noreply.github.com>
2021-02-11 16:26:38 -0500

    firstTwelveNoteCommit

---
+++ TwelveNote.WebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TwelveNote.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ TwelveNote.WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TwelveNote.WebAPI.WebApiApplication" Language="C#" %>
