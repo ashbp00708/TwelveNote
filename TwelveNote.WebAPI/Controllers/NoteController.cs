a58d9a7640ea4b94b242aa27f905c8056a546b73
ashbp00708 <73564911+ashbp00708@users.noreply.github.com>
2021-02-15 17:40:32 -0500

    After Update

--- TwelveNote.WebAPI/Controllers/NoteController.cs
+++ TwelveNote.WebAPI/Controllers/NoteController.cs
@@ -21,15 +21,15 @@ namespace TwelveNote.WebAPI.Controllers
         }
         public IHttpActionResult Post(NoteCreate note)
         {
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
+           if (!ModelState.IsValid)
+               return BadRequest(ModelState);
 
-            var service = CreateNoteService();
+           var service = CreateNoteService();
 
-            if (!service.CreateNote(note))
-                return InternalServerError();
+           if (!service.CreateNote(note))
+               return InternalServerError();
 
-            return Ok();
+           return Ok();
         }
         private NoteService CreateNoteService()
         {
@@ -51,6 +51,15 @@ namespace TwelveNote.WebAPI.Controllers
             var service = CreateNoteService();
 
             if (!service.UpdateNote(note))
+                return InternalServerError();
+
+            return Ok();
+        }
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateNoteService();
+
+            if (!service.DeleteNote(id))
                 return InternalServerError();
 
             return Ok();
